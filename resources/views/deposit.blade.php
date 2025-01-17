972ffead229ec06194ba18c18c23804c4a09059b
Jaykishan Rawat <jksonu1436@gmail.com>
2025-01-17 06:40:57 +0530

    done

--- resources/views/deposit.blade.php
+++ resources/views/deposit.blade.php
@@ -426,7 +426,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 
     <script>
         const minWithdraw = {{ $settings->minWithdraw }};
