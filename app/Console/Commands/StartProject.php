47bd0581e53c164095d83ccc120d6db4ec60db88
Jaykishan <jksonu1436@gmail.com>
2025-02-12 15:21:58 +0530

    Create Command

---
+++ app/Console/Commands/StartProject.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class StartProject extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dabba:chalu';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $commands = [
+            'php artisan optimize',
+
+            'docker-compose up -d',
+            'php artisan websockets:serve',
+            'php artisan serve',
+        ];
+
+        foreach ($commands as $command) {
+            $this->info("Running: $command");
+
+            $process = Process::fromShellCommandline($command);
+            $process->setTimeout(0); // No timeout
+            $process->run(function ($type, $buffer) {
+                echo $buffer;
+            });
+
+            if (!$process->isSuccessful()) {
+                $this->error("Failed: $command");
+            }
+        }
+
+        $this->info('All commands executed successfully.');
+    }
+}
